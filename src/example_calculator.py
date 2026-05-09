4ae219311d7696d614ebae44792ee466706a0eaa
BananaCollector <witold.banasiak@gmail.com>
2026-05-09 21:14:00 +0200

    Update example_calculator
    Robust module's testing

--- src/example_calculator.py
+++ src/example_calculator.py
@@ -7,13 +7,24 @@ def safe_division(a: float, b: float) ->float:
 
 def average(values: list[float]) -> float:
     if not values:
-        raise ValueError(" List cannot be empty!")
+        raise ValueError("List cannot be empty!")
     return(sum(values)/len(values))
 
 if __name__ == "__main__":
-    ex_1 = input("Enter two numbers to divide, separated by a space: ").split()
-    print(f"Your division: {safe_division(float(ex_1[0]), float(ex_1[1]))}")
+    ex_1 = input("Enter two numbers to divide, separated by a space (or 'd' for default): ").split()
     
-    ex_2 = input("Enter a list of numbers separated by spaces: ").split()
-    print(f"Your average: {average([float(x) for x in ex_2])}")
+    if ex_1[0] == "d":
+        print(f"Your division for 4 / 10: {safe_division(4, 10)}")
+    else:
+        print(f"Your division: {safe_division(float(ex_1[0]), float(ex_1[1]))}")
+    
+    ex_2 = input("Enter a list of numbers separated by spaces (or 'd' for default): ").split()
+
+    if not ex_2:
+            print(f"Your average: {average([])}")
+    else:
+        if ex_2[0] == 'd':
+            print(f"Your average for [10, 5, 7, 4]: {average([10, 5, 7, 4])}")
+        else:
+            print(f"Your average: {average([float(x) for x in ex_2])}")
     
